50e5ede9594e509b6347883a6fcbf1000012fdfb
Abelyudhoo <abel.ae813@gmail.com>
2024-10-17 18:26:54 +0700

    first commit

---
+++ MDK-ARM/RTOS_time/list.d
@@ -0,0 +1,12 @@
+rtos_time\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/list.c
+rtos_time\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+rtos_time\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+rtos_time\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+rtos_time\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+rtos_time\list.o: ../Core/Inc/FreeRTOSConfig.h
+rtos_time\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+rtos_time\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+rtos_time\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+rtos_time\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+rtos_time\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+rtos_time\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
